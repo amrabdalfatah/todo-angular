145254625d412897d876cd84e2b1d79f345b4553
amrabdalfatah <amr.abdalfatah.mohamed@gmail.com>
2022-09-20 23:53:57 +0200

    First creation of Data Models

---
+++ src/app/todoItem.ts
@@ -0,0 +1,10 @@
+export class TodoItem {
+    constructor(public task:string, public complete:boolean = false) {
+        // statement
+    }
+}
+
+// user - amr
+// go to gym --> true
+// buy milk --> false
+// study --> false
